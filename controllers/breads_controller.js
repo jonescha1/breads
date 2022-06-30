3aaa6e19509603f54d7b21796dabe56fd8c87758
Chase Jones <chaseejones@icloud.com>
2022-06-30 19:34:29 -0400

    Sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,12 +1,18 @@
 // DEPENDENCIES
 const express = require("express");
+const Bread = require("../models/bread");
 
 // CONFIGURATION
 const breads = express.Router();
 
 // INDEX
 breads.get("/", (req, res) => {
-  res.send("This is the index at /breads");
+  res.send(Bread);
+});
+
+// SHOW
+breads.get("/:arrayIndex", (req, res) => {
+  res.send(Bread[req.params.arrayIndex]);
 });
 
 module.exports = breads;
