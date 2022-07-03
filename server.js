a38c168f113ef40ef8fa7d3d58eb22ec95651fb9
Chase Jones <chaseejones@icloud.com>
2022-07-03 10:37:41 -0400

    Added 404 page and added static public folder to the middleware section

--- server.js
+++ server.js
@@ -5,21 +5,27 @@ const chalk = require("chalk");
 // CONFIGURATION
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 8000; // Port variable that in retrieved from the .env file.
+const PORT = process.env.PORT || 8000; // Port variable that is retrieved from the .env file.
 
 // MIDDLEWARE
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to an App about Breads!");
 });
 
-// Breads
+// BREADS ROUTE
 const breadsController = require("./controllers/breads_controller");
 app.use("/breads", breadsController);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 // Listen
 app.listen(PORT, () => {
