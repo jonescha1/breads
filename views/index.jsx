6190e0d90815b94153ea4f3fcf1a39ebbd08f0b3
Chase Jones <chaseejones@icloud.com>
2022-07-03 11:37:59 -0400

    corrected href

--- views/index.jsx
+++ views/index.jsx
@@ -5,12 +5,11 @@ function Index({ breads, title }) {
   return (
     <Default title={title}>
       <h2>Index Page</h2>
-      {/*<p>I have {breads[0].name} bread!</p>*/}
       <ul>
         {breads.map((bread, index) => {
           return (
             <li key={index}>
-              <a href="{`/breads/${index}`}">{bread.name}</a>
+              <a href={`/breads/${index}`}>{bread.name}</a>
             </li>
           );
         })}
