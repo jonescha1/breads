84d27f0a0a3c8c18ad6cac5b6796980f0ee219fc
Chase Jones <chaseejones@icloud.com>
2022-07-07 19:58:44 -0400

    Create and New routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -5,6 +5,12 @@ function Index({ breads, title }) {
   return (
     <Default title={title}>
       <h2>Index Page</h2>
+      <div className="newButton">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
+      </div>
+
       <ul>
         {breads.map((bread, index) => {
           return (
