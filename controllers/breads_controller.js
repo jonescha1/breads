e797e7de95b3c611b3e3f58872cfeb66f0a3509f
Chase Jones <chaseejones@icloud.com>
2022-07-07 21:10:34 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -39,6 +39,25 @@ breads.delete("/:indexArray", (req, res) => {
   res.status(303).redirect("/breads");
 });
 
+// UPDATE
+breads.put("/:arrayIndex", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
+  } else {
+    req.body.hasGluten = false;
+  }
+  Bread[req.params.arrayIndex] = req.body;
+  res.redirect(`/breads/${req.params.arrayIndex}`);
+});
+
+// EDIT
+breads.get("/:indexArray/edit", (req, res) => {
+  res.render("edit", {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray,
+  });
+});
+
 // SHOW
 breads.get("/:arrayIndex", (req, res) => {
   if (Bread[req.params.arrayIndex]) {
